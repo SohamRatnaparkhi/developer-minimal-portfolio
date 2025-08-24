60cea176937e25220e2059dde7530534cdb70344
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2025-08-24 14:20:13 +0530

    feat: update index.html and robots.txt for enhanced SEO and user experience; revise meta tags, add favicon links, and specify sitemap for crawlers

---
+++ scripts/generate-favicons.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+// This script generates favicon files from the SVG
+// You'll need to run this manually or use a tool like sharp/svgexport to convert SVG to PNG
+
+console.log('Favicon generation script');
+console.log('To generate PNG favicons from the SVG, you can:');
+console.log('1. Use an online SVG to PNG converter');
+console.log('2. Use a tool like sharp or svgexport');
+console.log('3. Use a design tool like Figma or Sketch');
+console.log('');
+console.log('Required favicon files:');
+console.log('- favicon-16x16.png (16x16)');
+console.log('- favicon-32x32.png (32x32)');
+console.log('- apple-touch-icon.png (180x180)');
+console.log('');
+console.log('The SVG favicon has been created at public/favicon.svg');
+console.log('You can also manually create these PNG files and place them in the public/ directory.');
